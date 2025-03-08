62df67361adcaba1ca67d6756321963e8479dc1f
timlok-Laptop <timch@LAPTOP-BDU11A8P>
2025-03-08 17:42:30 +0800

    Update the Handledata dir

--- HandleData/combine.sh
+++ HandleData/combine.sh
@@ -15,24 +15,52 @@ fi
 # 2. Loop through the content of different csv and combine the data
 
 echo -e "Now working in directory : $CURR_DIR \n"
-echo "Starting integer, Ending integer, Sum" > "$OUTPUT_CSV_File" # header 
+echo -e "Below are the files to be processed:"
+filelist=$(find $CURR_DIR -type f -regex "${CURR_DIR}/sum_[0-9]+_[0-9]+.csv")
+for file in $filelist
+do 
+    basename $file | xargs -I {} echo {}
+done 
+
+while true
+do
+    echo "Are you sure to continue? [y/n]"
+    read user_input
+    if [ "$user_input" == "y" -o "$user_input" == "Y" ]; then
+        break
+    elif [ "$user_input" == "n" ]; then
+        echo "The script will be terminated."
+        exit 0
+    else
+        echo "Invalid input. Please enter 'y' or 'n'."
+    fi
+done
+
 for file in $(find $CURR_DIR -type f -regex "${CURR_DIR}/sum_[0-9]+_[0-9]+.csv"); 
 do
     # Extract the base name from the full path
     basename=$(basename "$file")
 
     # Append the content of each CSV file, excluding the header, to the output file
-    echo "Processing the file ${basename}"
+    echo "Processing file ${basename}"
     tail -n +2 "${basename}" >> "$OUTPUT_CSV_File"
 
 done
-echo -e "\n"
 
 # Calculate the sum as well
 # -F set the separator to be comma 
-# END {print sum} - show the sum after processing all lines 
-column_sum=$(awk -F, '{sum += $3} END {print "The sum is " sum}' "$OUTPUT_CSV_File")
-#echo "The sum is ${column_sum}"
+# BEGIN - Print the message before the start of program 
+# NR > 1 - Process the data from row 1. Go through the file row by row. $3 refers to 3rd rows
+# END {print sum} - show the sum after processing all lines
+column_sum=$(
+    awk -F, '
+    NR > 0 { 
+        sum += $3 
+    }
+    END {
+    print "The sum is " sum 
+    }' "$OUTPUT_CSV_File" 
+)
 echo $column_sum >> $OUTPUT_CSV_File
 
 # show the content & info the combined file
---
+++ HandleData/explanation.md
@@ -0,0 +1,15 @@
+
+You are now given the csv where their names have the following format:
+
+`sum_<starting integer>_<ending integer>.csv`
+
+1. These csv records the sum of integers from `<starting integer>` to `<ending integer>`. 
+   Do the following 
+   - Combine the data in different csv to a new csv with name `sum_combined.csv` with
+   the same headers in each of the csv 
+   - Let the user check before combining the csv 
+   - Calculate the final sum. Store it as the last of the combined csv 
+   - Show the `sum_combined.csv` after the combination of csv.  
+
+2. Copy all the csv file with the format "sum_<starting integer>_<ending integer>.csv" 
+   in this directory to directory "./BackUp" 
